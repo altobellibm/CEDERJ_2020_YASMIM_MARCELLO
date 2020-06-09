8ce29b3d92eef1b080b02c58ed54c89650bec41e
Marcello Telles <marcello.telles.dev@gmail.com>
2020-06-08 21:07:25 -0300

    feat(anvisa): identificando coluna da Bula do Profissional

--- scrapy/anvisaspider.py
+++ scrapy/anvisaspider.py
@@ -2,14 +2,6 @@ import scrapy
 
 class AnvisaSpider(scrapy.Spider):
     name = 'anvisa'
-    #start_urls = [
-    #    'http://quotes.toscrape.com/tag/humor/',
-    #]
-    # request_with_cookies = Request(url="http://www.example.com",
-    #                            cookies=[{'name': 'currency',
-    #                                     'value': 'USD',
-    #                                     'domain': 'example.com',
-    #                                     'path': '/currency'}])
 
     def start_requests(self):
         form_data = {
@@ -30,20 +22,19 @@ class AnvisaSpider(scrapy.Spider):
 
     def request_callback(self, response):
         download_pdf_url = "http://www.anvisa.gov.br/datavisa/fila_bula/frmVisualizarBula.asp"
-        #print(response.css("#tblResultado tbody").getall())
-        for quote in response.css("#tblResultado tbody tr"): #tabela de resultado possui a ID tblResultado
-            print(quote.get())
-        # pensando em como baixar o PDF
-        # fVisualizarBula('6568522018', '10663733')
-        # bate em http://www.anvisa.gov.br/datavisa/fila_bula/frmVisualizarBula.asp
-        # com um POST recebendo FormData
-        #pNuTransacao: 6568522018 (1o parametro)
-        #pIdAnexo: 10663733 (2o parametro)
+        column_index = 1
+        target_column_text = "Bula do Profissional"
+        for table_headers_selector in response.css("#tblResultado thead tr th"): #tabela de resultado possui a ID tblResultado
+            table_header_as_string = table_headers_selector.get().replace("<br>", " ")
+            #print("table_header_text: "+table_header_as_string)
+            if target_column_text in table_header_as_string:
+                #print("achei no index", column_index)
+                break
+            else:
+                column_index += 1
+            print(table_header_as_string)
+        print(column_index)
 
-        #TODO: fazer primeiro uma requisição para identificar quantos registros são
-        #Ex.: fazer uma requisição com 1 por página. Se derem 5 páginas, vamos fazer uma nova requisição com 5 para capturar tudo.
-        #O bulário carrega as páginas seguintes com Javascript conforme o clique do botão, então é bem tranquilo e rápido fazer requisição
-        #com 1 registro por página.
 
     def parse(self, response):
         for quote in response.css('div.quote'):
