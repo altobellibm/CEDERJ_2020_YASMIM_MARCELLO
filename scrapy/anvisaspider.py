ba7e70ae36479f10993af5b6e18dbc4598f876f2
Marcello Telles <marcello.telles.dev@gmail.com>
2020-06-08 22:13:49 -0300

    feat(anvisa): baixando arquivos PDFs da primeira pagina

--- scrapy/anvisaspider.py
+++ scrapy/anvisaspider.py
@@ -1,60 +1,60 @@
 import scrapy
 import re
+import pdb
 
 class AnvisaSpider(scrapy.Spider):
     name = 'anvisa'
 
     def start_requests(self):
-        form_data = {
-            'txtMedicamento': 'dipirona',
-            'txtEmpresa': '',
-            'txtNuExpediente': '',
-            'txtDataPublicacaoI': '',
-            'txtDataPublicacaoF': '',
-            'txtPageSize': '3',
-            'btnPesquisar': ''
-        }
-        return [
-            scrapy.FormRequest("http://www.anvisa.gov.br/datavisa/fila_bula/frmResultado.asp",
-            formdata=form_data,
-            callback=self.request_callback)
-        ]
+        yield scrapy.FormRequest("http://www.anvisa.gov.br/datavisa/fila_bula/frmResultado.asp",
+            formdata={
+                'txtMedicamento': 'dipirona',
+                'txtEmpresa': '',
+                'txtNuExpediente': '',
+                'txtDataPublicacaoI': '',
+                'txtDataPublicacaoF': '',
+                'txtPageSize': '3',
+                'btnPesquisar': ''
+            },
+            callback=self.request_callback
+        )
                                    
-
     def request_callback(self, response):
-        download_pdf_url = "http://www.anvisa.gov.br/datavisa/fila_bula/frmVisualizarBula.asp"
-        column_index = self.get_column_index(response, "tblResultado", "Bula do Profissional")
+        self.logger.info()
+        table_element_id = "tblResultado"
+        column_index = self.get_column_index(response, table_element_id, "Bula do Profissional")
         
-        table_cells_css_path = "#tblResultado tbody tr td:nth-child("+str(column_index)+")"
-        print(table_cells_css_path)
+        table_cells_css_path = "#"+table_element_id+" tbody tr td:nth-child("+str(column_index)+")"
         for table_cells_selector in response.css(table_cells_css_path):
             file_link = table_cells_selector.css("a::attr(onclick)").get()
             file_arguments_list = self.get_file_arguments_list(file_link)
             transaction_number = self.get_transaction_number(file_arguments_list)
             attachment_number = self.get_attachment_number(file_arguments_list)
-            print(file_link+" -> "+str(transaction_number)+" and "+str(attachment_number))
-            #print(file_link)
-
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'author': quote.xpath('span/small/text()').get(),
-                'text': quote.css('span.text::text').get(),
-            }
-
-        # next_page = response.css('li.next a::attr("href")').get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, self.parse)
+            yield scrapy.FormRequest("http://www.anvisa.gov.br/datavisa/fila_bula/frmVisualizarBula.asp",
+                formdata={
+                    'pNuTransacao': transaction_number,
+                    'pIdAnexo': attachment_number
+                },
+                headers={'X-Attachment-Number': attachment_number},
+                callback=self.save_pdf
+            )
 
     def get_column_index(self, response, table_element_id, target_column_text):
+        self.logger.info("Procurando pelo titulo de coluna '%s'", target_column_text)
         column_index = 1
+        column_found = False
         for table_headers_selector in response.css("#"+table_element_id+" thead tr th"): #tabela de resultado possui a ID tblResultado
             table_header_as_string = table_headers_selector.get().replace("<br>", " ")
             if target_column_text in table_header_as_string:
+                column_found = True
                 break
             else:
                 column_index += 1
-        return column_index
+        if column_found:
+            self.logger.info("Coluna encontrada no indice %d", column_index)
+            return column_index
+        else:
+            raise Exception("Nao foi possivel encontrar a coluna '%s'", target_column_text)
 
     def get_file_arguments_list(self, onclick_function):
         return re.findall("\d+", onclick_function)
@@ -64,3 +64,10 @@ class AnvisaSpider(scrapy.Spider):
 
     def get_attachment_number(self, file_arguments_list):
         return file_arguments_list[1]
+
+    def save_pdf(self, response):
+        filename = response.request.headers['X-Attachment-Number']
+        path = filename + ".pdf"
+        self.logger.info('Salvando PDF %s', path)
+        with open(path, 'wb') as f:
+            f.write(response.body)
