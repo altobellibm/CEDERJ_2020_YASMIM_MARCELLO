e40866cb61afe1b706e4eaf27fe8b1660faabc9b
Marcello Telles <marcello.telles@visagio.com>
2020-06-22 01:20:20 -0300

    feat: usando parametro especifico do PDFMiner para interpretar os arquivos

--- pdfreader/main.py
+++ pdfreader/main.py
@@ -11,10 +11,15 @@ CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 def convert_pdf_to_txt(path):
     pdf_resource_manager = PDFResourceManager()
     retstr = StringIO()
-    device = TextConverter(pdf_resource_manager, retstr, codec='utf-8', laparams=LAParams(line_margin=0.1)) #especificando o parametro line_margin para garantir a ordem correta de interpretacao do PDF
+    laparams = LAParams(
+        line_margin=5 #default 0.3
+    )
+    device = TextConverter(pdf_resource_manager, retstr, codec='utf-8', laparams=laparams) #especificando o parametro line_margin para garantir a ordem correta de interpretacao do PDF
     with open(path, 'rb') as fp:
         pdf_page_interpreter = PDFPageInterpreter(pdf_resource_manager, device)
-        for page in PDFPage.get_pages(fp, set(), maxpages=0, password="", caching=True, check_extractable=True):
+        #pagenos = [5]
+        pagenos = []
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=0, password="", caching=True, check_extractable=True):
             pdf_page_interpreter.process_page(page)
         text = retstr.getvalue()
     device.close()
@@ -30,17 +35,20 @@ for filename in os.listdir(files_dir):
             pdf_text_content = convert_pdf_to_txt(os.path.abspath(os.path.join(files_dir, filename)))
             print('PDF lido')
             composition_occurrences_amount = pdf_text_content.count('COMPOSIÇÃO')
+            technical_info_occurrences_amount = pdf_text_content.count('INFORMAÇÕES TÉCNICAS')
             composition_start_index = 0
             composition_end_index = 0
-            for i in range(composition_occurrences_amount):
-                composition_start_index = pdf_text_content.find('COMPOSIÇÃO', composition_start_index)
+            for i in range(min(composition_occurrences_amount, technical_info_occurrences_amount)):
+                composition_start_index = pdf_text_content.find('COMPOSIÇÃO', composition_start_index+1)
                 print('Range start: ' + str(composition_start_index))
-                composition_end_index = pdf_text_content.find('INFORMAÇÕES TÉCNICAS', composition_start_index)
+                composition_end_index = pdf_text_content.find('INFORMAÇÕES TÉCNICAS', composition_start_index+1)
                 print('Range end: ' + str(composition_end_index))
-                #if composition_end_index < composition_start_index:
-                #    #se cairmos aqui eh devido ao 'informacoes tecnicas' vir antes da 'composicao'
-                #    #neste caso, devemos procurar por 'indicacoes' para delimitar o fim da secao desejada
-                #    composition_end_index = pdf_text_content.find('INDICAÇÕES')
-                #    print('Range end ajustado: ' + str(composition_end_index))
-                f.write(pdf_text_content[composition_start_index : composition_end_index])
+                if composition_end_index < composition_start_index:
+                    #se cairmos aqui eh devido ao 'informacoes tecnicas' vir antes da 'composicao'
+                    #neste caso, devemos procurar por 'indicacoes' para delimitar o fim da secao desejada
+                    composition_end_index = pdf_text_content.find('INDICAÇÕES')
+                    print('Range end ajustado: ' + str(composition_end_index))
+                if composition_end_index > composition_start_index:
+                    #se apos a verificacao pelos fins de secao alguma for bem sucedida, o trecho eh valido
+                    f.write(pdf_text_content[composition_start_index : composition_end_index])
 #print(convert_pdf_to_txt(my_file))
