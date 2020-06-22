1bbf6df2389465644242a2511e5d296b71312929
Marcello Telles <marcello.telles@visagio.com>
2020-06-21 23:44:57 -0300

    feat: especificando parametro na classe LAParams
    objetivo é garantir a interpretação correta da ordem do contéudo dos PDFs

--- pdfreader/main.py
+++ pdfreader/main.py
@@ -11,7 +11,7 @@ CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 def convert_pdf_to_txt(path):
     pdf_resource_manager = PDFResourceManager()
     retstr = StringIO()
-    device = TextConverter(pdf_resource_manager, retstr, codec='utf-8', laparams=LAParams())
+    device = TextConverter(pdf_resource_manager, retstr, codec='utf-8', laparams=LAParams(line_margin=0.1)) #especificando o parametro line_margin para garantir a ordem correta de interpretacao do PDF
     with open(path, 'rb') as fp:
         pdf_page_interpreter = PDFPageInterpreter(pdf_resource_manager, device)
         for page in PDFPage.get_pages(fp, set(), maxpages=0, password="", caching=True, check_extractable=True):
@@ -22,22 +22,25 @@ def convert_pdf_to_txt(path):
     return text
 
 files_dir = os.path.join(CURRENT_PATH, os.pardir, "scrapy", "bula_download")
-#my_file = os.path.join(files_dir, '2462371.pdf')
-composition_start_index = -1
-with open('pdf.txt', 'w', encoding='utf-8') as f:
-    for filename in os.listdir(files_dir):
-        f.write('**** Arquivo ' + filename + ' ****')
-        print('**** Arquivo ' + filename + ' ****')
-        pdf_text_content = convert_pdf_to_txt(os.path.abspath(os.path.join(files_dir, filename)))
-        print('PDF lido')
-        for i in range(pdf_text_content.count('COMPOSIÇÃO')):
-            composition_start_index = pdf_text_content.find('COMPOSIÇÃO')
-            print('Range start: ' + composition_start_index)
-            composition_end_index = pdf_text_content.find('INFORMAÇÕES TÉCNICAS')
-            print('Range end: ' + composition_end_index)
-            if composition_end_index < composition_start_index:
-                #se cairmos aqui eh devido ao 'informacoes tecnicas' vir antes da 'composicao'
-                #neste caso, devemos procurar por 'indicacoes' para delimitar o fim da secao desejada
-                composition_end_index = pdf_text_content.find('INDICAÇÕES')
-            f.write(pdf_text_content[composition_start_index : composition_end_index])
+for filename in os.listdir(files_dir):
+    if filename == '4922996.pdf':
+        with open('pdf'+filename+'.txt', 'w', encoding='utf-8') as f:
+            f.write('\n**** Arquivo ' + filename + ' ****\n')
+            print('**** Arquivo ' + filename + ' ****')
+            pdf_text_content = convert_pdf_to_txt(os.path.abspath(os.path.join(files_dir, filename)))
+            print('PDF lido')
+            composition_occurrences_amount = pdf_text_content.count('COMPOSIÇÃO')
+            composition_start_index = 0
+            composition_end_index = 0
+            for i in range(composition_occurrences_amount):
+                composition_start_index = pdf_text_content.find('COMPOSIÇÃO', composition_start_index)
+                print('Range start: ' + str(composition_start_index))
+                composition_end_index = pdf_text_content.find('INFORMAÇÕES TÉCNICAS', composition_start_index)
+                print('Range end: ' + str(composition_end_index))
+                #if composition_end_index < composition_start_index:
+                #    #se cairmos aqui eh devido ao 'informacoes tecnicas' vir antes da 'composicao'
+                #    #neste caso, devemos procurar por 'indicacoes' para delimitar o fim da secao desejada
+                #    composition_end_index = pdf_text_content.find('INDICAÇÕES')
+                #    print('Range end ajustado: ' + str(composition_end_index))
+                f.write(pdf_text_content[composition_start_index : composition_end_index])
 #print(convert_pdf_to_txt(my_file))
