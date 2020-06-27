bcdb91ad23869c4456014da10e05cbbe19f9e834
Marcello Telles <marcello.telles@visagio.com>
2020-06-26 21:27:43 -0300

    feat: limpando arquivos traduzidos antes de rodar

--- excipient_scraper/pdfreader/handbook/main.py
+++ excipient_scraper/pdfreader/handbook/main.py
@@ -26,4 +26,14 @@ def translate_excipients(input_files_dir, output_files_dir):
                 output_file.write(json.dumps(original_json, indent=4))
             print('Arquivo ', file.name, ' traduzido')
 
+def clean_folder(path):
+    if path.exists():
+        if path.is_dir():
+            files = path.glob('**/*')
+            for f in files:
+                if f.is_file():
+                    f.unlink()
+
+output_files_dir = CURRENT_FILE_PATH / "translated_content"
+clean_folder(output_files_dir)
 translate_excipients(CURRENT_FILE_PATH.parent / "bulas" / "pdf_content", CURRENT_FILE_PATH / "translated_content")
