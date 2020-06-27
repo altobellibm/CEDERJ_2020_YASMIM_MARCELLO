0adff825e234410d862fa42cdf8298555a1dc4b4
Marcello Telles <marcello.telles@visagio.com>
2020-06-27 13:53:02 -0300

    feat: transformando estrutura em classes

--- excipient_scraper/pdfreader/handbook/main.py
+++ excipient_scraper/pdfreader/handbook/main.py
@@ -1,39 +1,8 @@
-from googletrans import Translator
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
-from pathlib import Path
-import os
-import pdb
-import json
-import re
+from translationmanager import TranslationManager
+from summaryparser import SummaryParser
 
-CURRENT_FILE_PATH = Path(__file__).parent
+#translation_manager = TranslationManager()
+#translation_manager.translate()
 
-def translate_excipients(input_files_dir, output_files_dir):
-    translator = Translator()
-    for file in input_files_dir.glob('*'):
-        if file.is_file():
-            with open(file, 'r') as input_file:
-                original_json = json.loads(input_file.read())
-            translated_excipientes = translator.translate(original_json['excipientes'], src='pt', dest='en')
-            original_json['excipientes_ingles'] = translated_excipientes.text
-            if not output_files_dir.exists():
-                Path.mkdir(output_files_dir)
-            with open(output_files_dir / file.name, 'w') as output_file:
-                output_file.write(json.dumps(original_json, indent=4))
-            print('Arquivo ', file.name, ' traduzido')
-
-def clean_folder(path):
-    if path.exists():
-        if path.is_dir():
-            files = path.glob('**/*')
-            for f in files:
-                if f.is_file():
-                    f.unlink()
-
-output_files_dir = CURRENT_FILE_PATH / "translated_content"
-clean_folder(output_files_dir)
-translate_excipients(CURRENT_FILE_PATH.parent / "bulas" / "pdf_content", CURRENT_FILE_PATH / "translated_content")
+summary_parser = SummaryParser()
+summary_parser.parse()
---
+++ excipient_scraper/pdfreader/handbook/summaryparser.py
@@ -0,0 +1,62 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+from pathlib import Path
+import os
+import pdb
+import json
+import re
+
+class SummaryParser:
+    def convert_pdf_to_txt(self, path, pagenos=[]):
+        pdf_resource_manager = PDFResourceManager()
+        retstr = StringIO()
+        laparams = LAParams(
+            line_margin=3, #default 0.3
+            char_margin=35 #default 2.0
+        )
+        device = TextConverter(pdf_resource_manager, retstr, codec='utf-8', laparams=laparams) #especificando o parametro line_margin para garantir a ordem correta de interpretacao do PDF
+        with open(path, 'rb') as fp:
+            pdf_page_interpreter = PDFPageInterpreter(pdf_resource_manager, device)
+            for page in PDFPage.get_pages(fp, pagenos, maxpages=0, password="", caching=True, check_extractable=True):
+                pdf_page_interpreter.process_page(page)
+            text = retstr.getvalue()
+        device.close()
+        retstr.close()
+        return text
+
+    def write_to_file(self, path, mode, content):
+        folder = path.parent
+        if not folder.exists():
+            Path.mkdir(folder)
+        with open(path, mode, encoding='utf-8') as f:
+            f.write(content)
+
+    def parse(self):
+        #pagina real = pagina do sumario + 29
+        summary_pages = [5, 6, 7, 8, 9]
+        summary = self.convert_pdf_to_txt(Path(__file__).parent / 'Handbook-of-Pharmaceutical-Excipients_6t.pdf', summary_pages)
+        print(len(summary), 'caracteres lidos do sumario')
+        summary_garbage_end_string = 'Monographs\n'
+        summary_garbage_end_index = summary.find(summary_garbage_end_string)
+        summary = summary[(summary_garbage_end_index + len(summary_garbage_end_string)):]
+        self.write_to_file(Path(__file__).parent / 'summary.txt', 'w', summary)
+        summary_dict = {}
+        real_page_offset = 29
+        with open(Path(__file__).parent / 'summary.txt', 'r', encoding='utf-8') as f:
+            for line in f.readlines():
+                if not 'Appendix' in line and not 'Contents' in line:
+                    line = line.rstrip('\n').strip('\f')
+                    last_space_index = line.rfind(' ')
+                    excipient = line[:last_space_index]
+                    pageno = line[last_space_index+1:]
+                    if len(excipient) > 0 and len(pageno) > 0:
+                        summary_dict[excipient] = pageno
+        print('Apendices e cabecalhos removidos')
+
+        with open(Path(__file__).parent / 'summary.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(summary_dict, indent=4))
+
+        print('Fim da conversao do sumario. Resultado em', Path(__file__).parent / 'summary.json')
