44139e8a7d76d7f20e12abbe7c29d394728f0011
Marcello Telles <marcello.telles@visagio.com>
2020-06-29 15:06:56 -0300

    preenchendo colunas com não por padrão

--- excipient_scraper/stats.py
+++ excipient_scraper/stats.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import json
+import datetime as dt
 from pathlib import Path
 
 bula_download = Path(__file__).parent / 'scrapy' / 'bula_download'
 bulas_content = Path(__file__).parent / 'pdfreader' / 'bulas_content'
-df = pd.DataFrame(columns=['Medicamento', 'Bula', 'Formulação', 'Excipientes'])
+df = pd.DataFrame(columns=['Medicamento', 'Bula', 'Formulação', 'Formulação encontrada?', 'Excipientes', 'Excipientes encontrados?'])
 for folder in bula_download.glob('*'):
     if folder.is_dir():
         for file in folder.glob('*'):
             if file.is_file():
-                record = {'Medicamento': folder.name, 'Bula': file.name}
+                record = {
+                    'Medicamento': folder.name,
+                    'Bula': file.name,
+                    'Formulação encontrada?': 'Não',
+                    'Excipientes encontrados?': 'Não'
+                }
                 parsed_content_file = (bulas_content / folder.name / file.stem).with_suffix('.json')
                 if parsed_content_file.exists() and parsed_content_file.is_file():
                     with open(parsed_content_file, 'r', encoding='utf-8') as parsed:
@@ -19,13 +25,19 @@ for folder in bula_download.glob('*'):
                             parsed_content = ''
                     if parsed_content:
                         for json_record in parsed_content:
-                            record['Formulação'] = json_record['formulacao']
-                            record['Excipientes'] = json_record['excipientes']
+                            formulation = json_record['formulacao']
+                            record['Formulação'] = formulation
+                            if formulation:
+                                record['Formulação encontrada?'] = 'Sim'
+                            excipients = json_record['excipientes'].lstrip('[').rstrip(']')
+                            record['Excipientes'] = excipients
+                            if excipients:
+                                record['Excipientes encontrados?'] = 'Sim'
                             df = df.append(record, ignore_index=True)
                     else:
                         df = df.append(record, ignore_index=True)
 
-with pd.ExcelWriter('output.xlsx') as writer:  
+with pd.ExcelWriter((Path(__file__).parent / 'output').with_suffix('.xlsx')) as writer:  
     df.to_excel(writer, sheet_name='Visão Bulas')
 
 stop = 0
